dce4f84c7431a9514ef743a88cbb5ded63b51231
Seungup Noh <soungho70_@naver.com>
2021-07-31 18:47:04 +0900

    ADD 애플리케이션의 기본 구성 설정을 담은 클래스 추가
    closes #1

---
+++ Application.py
@@ -0,0 +1,95 @@
+from typing import (
+    Optional,
+    Callable,
+    Any,
+    Union,
+    Type,
+    Dict
+)
+
+import uvloop
+from sanic import (
+    Sanic,
+    Request
+)
+
+from sanic.config import (
+    Config,
+    SANIC_PREFIX
+)
+
+from sanic.handlers import ErrorHandler
+from sanic.router import Router
+from sanic.signals import SignalRouter
+
+from Config.Application.ApplicationConfig import ApplicationConfig
+
+from Database.DatabaseManager import DatabaseManager
+from Routing.Root.RootRouting import RootRouting
+
+
+class Application(Sanic):
+
+    def __init__(
+            self,
+            config: Optional[Config] = None,
+            ctx: Optional[Any] = None,
+            router: Optional[Router] = None,
+            signal_router: Optional[SignalRouter] = None,
+            error_handler: Optional[ErrorHandler] = None,
+            load_env: Union[bool, str] = True,
+            env_prefix: Optional[str] = SANIC_PREFIX,
+            request_class: Optional[Type[Request]] = None,
+            strict_slashes: bool = False,
+            log_config: Optional[Dict[str, Any]] = None,
+            configure_logging: bool = True,
+            register: Optional[bool] = None,
+            dumps: Optional[Callable[..., str]] = None,
+    ) -> None:
+        # Config Application
+        self.application_config = ApplicationConfig.get_instance()
+
+        super().__init__(
+            name=self.application_config.APP_NAME,
+            config=config,
+            ctx=ctx,
+            router=router,
+            signal_router=signal_router,
+            error_handler=error_handler,
+            load_env=load_env,
+            env_prefix=env_prefix,
+            request_class=request_class,
+            strict_slashes=strict_slashes,
+            log_config=log_config,
+            configure_logging=configure_logging,
+            register=register,
+            dumps=dumps
+        )
+
+    def start(self):
+        self.set_events()
+        self.set_routing()
+        self.run(
+            host=self.application_config.HOST,
+            port=self.application_config.PORT
+        )
+
+    def set_events(self):
+        @self.listener('before_server_start')
+        async def before_server_start(app: Sanic, loop: uvloop.Loop):
+            app.ctx.database_manager = DatabaseManager.get_instance()
+
+        @self.listener('after_server_start')
+        async def after_server_start(app: Sanic, loop: uvloop.Loop):
+            ...
+
+        @self.listener('before_server_stop')
+        async def before_server_start(app: Sanic, loop: uvloop.Loop):
+            ...
+
+        @self.listener('after_server_stop')
+        async def after_server_start(app: Sanic, loop: uvloop.Loop):
+            ...
+
+    def set_routing(self):
+        RootRouting.set_routing(self)
