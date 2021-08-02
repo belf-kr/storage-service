6209de3cdd4244a62c1e598da5b673b0f07fe823
Seungup Noh <soungho70_@naver.com>
2021-08-02 11:38:53 +0900

    ADD 편의기능 추가
    close #6

--- Utiles/SQLHelper.py
+++ Utiles/SQLHelper.py
@@ -1,2 +1,12 @@
+from Config.DatabaseConfig import DatabaseConfig
+
+
 class SQLHelper:
-    ...
+    def __init__(self):
+        DB_CONFIG = DatabaseConfig.get_instance()
+        self.mysql_connection_url = f"mysql://" \
+                                    f"{DB_CONFIG.USER}:{DB_CONFIG.PASSWORD}" \
+                                    f"@" \
+                                    f"{DB_CONFIG.HOST}:{DB_CONFIG.PORT}" \
+                                    f"/" \
+                                    f"{DB_CONFIG.DB}"
