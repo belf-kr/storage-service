06a60c004a6ec30f6b9a976206f3a802c02329eb
Seungup Noh <soungho70_@naver.com>
2021-08-07 00:05:30 +0900

    UP 파일 모델 추가
    close #6

--- models/File.py
+++ models/File.py
@@ -1,13 +1,15 @@
 from tortoise.models import Model
-from tortoise.fields import CharField, IntField
+from tortoise.fields import CharField, UUIDField
 
 
 class File(Model):
-    id = IntField(pk=True)
+    id = UUIDField(pk=True)
     file_name = CharField(100)
     original_file_name = CharField(255)
-    directory_path = CharField(500)
-    extension = CharField(10)
+    mimetypes = CharField(100)
+
+    class Meta:
+        tablename = "file"
 
     def __str__(self):
         return self.original_file_name
