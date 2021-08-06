180b2e2d6d1cdfcd6dc73cafa1f8d89bf7e2c790
Seungup Noh <soungho70_@naver.com>
2021-08-07 00:08:03 +0900

    ADD 파일 메니저 생성
    closes #6

---
+++ Manager/FileManager.py
@@ -0,0 +1,17 @@
+import aiofiles
+
+from sanic.request import File
+
+from Common.CommonDefines import CommonDefines
+from models.File import File as FileModel
+
+
+class FileManager:
+    @staticmethod
+    async def write(file: File, file_model: FileModel) -> None:
+        file_path = f"{CommonDefines.get_instance().UPLOAD_PATH}/{file_model.file_name}"
+        async with aiofiles.open(file_path, 'wb') as fp:
+            await fp.write(file.body)
+        await fp.close()
+
+
