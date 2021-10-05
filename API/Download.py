1132f6578e75bacfdb163ee93c76c26aa5931150
JP3Pe <rlaworhkd430430@gmail.com>
2021-10-05 15:49:58 +0900

    add: File 객체를 Base64, Base64 인코딩 된 html 요소로 가져오는 Endpoint 추가
    Base64 endpoint 기능은 수정 필요

--- API/Download.py
+++ API/Download.py
@@ -1,11 +1,10 @@
 from uuid import UUID
 
 from sanic import Blueprint
+from sanic_gzip import Compress
 
 from Manager.Response.DownloadResponse.DownloadResponseManager import DownloadResponseManager
 from Models.File import File as FileModel
-
-from sanic_gzip import Compress
 
 download = Blueprint(name="api_download", url_prefix="/download")
 compress = Compress()
@@ -16,3 +15,17 @@ compress = Compress()
 async def download_by_id(_, file_id: UUID):
     file_model = await FileModel.get(id=file_id)
     return await DownloadResponseManager.file_download_stream_message(file_model)
+
+
+@download.get("/base64/<file_id:uuid>")
+@compress.compress()
+async def download_base64_by_id(_, file_id: UUID):
+    file_model = await FileModel.get(id=file_id)
+    return await DownloadResponseManager.file_download_to_base64_message(file_model)
+
+
+@download.get("/base64/html/<file_id:uuid>")
+@compress.compress()
+async def download_html_element_by_id(_, file_id: UUID):
+    file_model = await FileModel.get(id=file_id)
+    return await DownloadResponseManager.file_download_to_html_element_from_base64_message(file_model)
