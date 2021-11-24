706b7f0dd21af6f70ad0305bb8eb35caf801bdf3
Seungup Noh <soungho70_@naver.com>
2021-11-25 00:12:23 +0900

    up: file delete 요청시 예외처리 추가

--- API/Delete.py
+++ API/Delete.py
@@ -1,7 +1,10 @@
+from http import HTTPStatus
+
 from sanic import Blueprint, Request
-from sanic.response import empty
+from sanic.response import empty, json
 from sanic_gzip import Compress
 
+from Common.UrlTool import get_query_string
 from Manager.File.FileManager import FileManager
 
 delete = Blueprint(name="api_delete", url_prefix="/delete")
@@ -11,6 +14,31 @@ compress = Compress()
 @delete.delete("/")
 @compress.compress()
 async def file_delete(request: Request):
-    await FileManager.delete_file_from_request(request)
+    """
+    Delete logical and physical file data
+    :param request:
+    :return:
+    """
+    query_string = get_query_string(request)
 
-    return empty(status=200)
+    user_id = query_string.get("userId")
+    file_id = query_string.get("fileId")
+
+    status = HTTPStatus.OK
+
+    if not user_id or not file_id:
+        return empty(status=HTTPStatus.BAD_REQUEST)
+
+    error_code = await FileManager.delete_file_model(file_id, user_id)
+
+    if error_code.is_success():
+        await FileManager.delete_file(file_id)
+    else:
+        status = HTTPStatus.INTERNAL_SERVER_ERROR
+
+    return json(
+        body={
+            "message": error_code.get_error_message()
+        },
+        status=status
+    )
