0fd7303e93689eff3a46561fe8fa317ccc89db2c
Seungup Noh <soungho70_@naver.com>
2021-11-27 05:08:15 +0900

    up: /downlod 엔드포인트 동작방식 변경

--- API/Download.py
+++ API/Download.py
@@ -3,24 +3,31 @@ from uuid import UUID
 
 from sanic import Blueprint
 from sanic.response import empty, file_stream
+from sanic.request import Request
 from sanic_gzip import Compress
 
+from Common import converter
+from Common.Request import Query
 from Models import FileModel
+from tortoise.exceptions import DoesNotExist
 
 download = Blueprint(name="api_download", url_prefix="/download")
 compress = Compress()
 
 
-@download.get("/<file_id:uuid>")
+@download.get("/")
 @compress.compress()
-async def download_by_id(_, file_id: UUID):
+async def download_by_id(request: Request):
     """
     Get physical binary data
-    :param _: no use
-    :param file_id:
+    :param request:
     :return:
     """
-    file_model = await FileModel.get(id=file_id)
+    try:
+        queries = converter.query_string_to_dict(request.query_string)
+        file_model = await FileModel.get(id=queries.get(Query.FILE_ID.value))
+    except DoesNotExist:
+        file_model = None
 
     if not file_model:
         return empty(status=HTTPStatus.NOT_FOUND)
