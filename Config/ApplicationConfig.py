b621a6cfb0845faca44c9cda1d01cec88b323401
jp3pe <rlaworhkd430430@gmail.com>
2023-05-02 09:39:40 +0900

    up: 버전 정보 업그레이드

--- Config/ApplicationConfig.py
+++ Config/ApplicationConfig.py
@@ -11,7 +11,7 @@ class ApplicationConfig:
         super().__init__()
 
         self.APP_NAME = "STORAGE_SERVICE"
-        self.VERSION = "1.1.0"
+        self.VERSION = "1.1.1"
         self.HOST = "0.0.0.0"
         if bool(getenv("STORAGE_SERVICE_IS_PROD")):
             self.PORT = 8000
